2468564bee2fee4bd9e2c2e76ba98013e3ba1257
fbergaut <f.bergaut@gmail.com>
2021-06-22 15:02:45 +0200

    Learning Props

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,15 +1,33 @@
 import { plantList } from "../datas/plantList";
+import PlantItem from "./PlantItem";
+import "../styles/ShoppingList.css";
 
 function ShoppingList() {
+  const categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  );
+
   return (
-    <ul>
-      {plantList.forEach((plant) => (
-        <li key={plant.id}>
-          {plant.category}
-          {plant.name}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
+        ))}
+      </ul>
+      <ul className="lmj-plant-list">
+          {plantList.map(({ id, cover, name, water, light }) => (
+            <PlantItem
+              id={id}
+              cover={cover}
+              name={name}
+              water={water}
+              light={light}
+            />
+          ))}
+      </ul>
+    </div>
   );
 }
 
